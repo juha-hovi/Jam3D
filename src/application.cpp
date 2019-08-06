1865a90bfb4506e972ae6fa7d16d4935ab0b35b8
juha-hovi <31245420+juha-hovi@users.noreply.github.com>
2019-08-06 11:37:22 +0900

    Added glDebugCallback

--- src/application.cpp
+++ src/application.cpp
@@ -35,6 +35,20 @@
                 - Draw object by clicking mouse
 */
 
+void GLAPIENTRY openGLDebugCallback(GLenum source,
+                                    GLenum type,
+                                    GLuint id,
+                                    GLenum severity,
+                                    GLsizei length,
+                                    const GLchar* message,
+                                    const void* userParam)
+{
+    std::cout << "[GL CALLBACK]: " << (type == GL_DEBUG_TYPE_ERROR ? "**GL ERROR**" : "")
+              << "(Type): " << type << "\n" 
+              << "(Severity): " << severity << "\n" 
+              << "(Message): " << message << std::endl;
+}
+
 int main() 
 {
     /////////////////////////////////
@@ -63,6 +77,9 @@ int main()
         std::cout << "Error: " << glewGetErrorString(err) << std::endl;
         return 0;
     }
+
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(openGLDebugCallback, 0);
 
     std::cout << "GLFW & GLEW initialized" << "\n" << "GLEW: " << glewGetString(GLEW_VERSION) << "\n"
               << "OpenGL: " << glGetString(GL_VERSION) << std::endl;
